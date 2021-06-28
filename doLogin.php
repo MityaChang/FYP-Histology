a13dcfc7c1564eb6167629658734791c8fd07181
MityaChang <qikai1998@gmail.com>
2021-06-28 17:34:29 +0800

    Perform a test

--- doLogin.php
+++ doLogin.php
@@ -1,21 +1,23 @@
 <?php
+//
+//if (isset($_POST["submit"])) {
+//
+//    $username = $_POST["login"];
+//    $pass = $_POST["password"];
+//
+//    require_once 'dbFunctions.php';
+//    require_once 'functions.php';
+//
+//    if (emptyIpnutLogin($username, $pass) != false) {
+//        header("location: login.php?error=emptyinput");
+//        exit();
+//    }
+//
+//    loginUser($link, $username, $pass);
+//} else {
+//    header("location: HomePage.php");
+//    exit();
+//}
 
-if (isset($_POST["submit"])) {
-
-    $username = $_POST["login"];
-    $pass = $_POST["password"];
-
-    require_once 'dbFunctions.php';
-    require_once 'functions.php';
-
-    if (emptyIpnutLogin($username, $pass) != false) {
-        header("location: login.php?error=emptyinput");
-        exit();
-    }
-
-    loginUser($link, $username, $pass);
-} else {
-    header("location: HomePage.php");
-    exit();
-}
+echo 'result '.$_POST['test'];
 ?>
